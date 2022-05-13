669b0f0cba14caf72b40255690ba22346438d56f
MichiMichiRu <michimichiru75@gmail.com>
2022-05-13 16:14:32 +0300

    Pause work on 1672

--- RichestCustomerWealth/RichestCustomerWealth/Program.cs
+++ RichestCustomerWealth/RichestCustomerWealth/Program.cs
@@ -4,30 +4,43 @@ namespace RichestCustomerWealth
 {
     class Program
     {
+
+        /*int MaximumWealth(int[][] accounts)
+        {
+            int[][] nums = accounts;
+
+            int maxSumm;
+
+
+            return maxSumm;
+        }*/
+
         static void Main(string[] args)
         {
+            int dim0 = 5;
+            int dim1 = 10;
+            int dim2 = 6;
 
-            int[,] x = { { 1, 2 }, { 3, 4 } };
-
-            int rows = x.GetUpperBound(0)+1;
-            int columns = x.Length / rows;
-            
-
-            for (int i = 0; i < columns; i++)
+            int[][][] threeDimArray = new int[dim0][][];
+            for (int idx0 = 0; idx0 < dim0; ++idx0)
             {
-                for (int j = 0; j < rows; j++)
+                int[][] twoDimArray = new int[dim1][];
+                for (int idx1 = 0; idx1 < dim1; ++idx1)
                 {
-
-                    Console.Write($"{x[i, j]} \t");
+                    int[] oneDimArray = new int[dim2];
+                    for (int idx2 = 0; idx2 < dim2; ++idx2)
+                    {
+                        oneDimArray[idx2] = idx0 * idx1 * idx2;
+                        Console.Write(idx2);
+                    }
+                    twoDimArray[idx1] = oneDimArray;
+                    Console.WriteLine(idx1);
                 }
-                Console.WriteLine();
-
+                threeDimArray[idx0] = twoDimArray;
+                Console.WriteLine(idx0);
             }
 
-            /*foreach (int i in x)
-            {
-                
-            }*/
+
         }
     }
 }
