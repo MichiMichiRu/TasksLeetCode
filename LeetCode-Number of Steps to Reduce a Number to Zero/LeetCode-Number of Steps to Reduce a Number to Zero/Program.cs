6089e3032d43c4658be563436e3b9742857d83f1
MichiMichiRu <michimichiru75@gmail.com>
2022-05-13 15:09:36 +0300

    Done task 1342

---
+++ LeetCode-Number of Steps to Reduce a Number to Zero/LeetCode-Number of Steps to Reduce a Number to Zero/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace LeetCode_Number_of_Steps_to_Reduce_a_Number_to_Zero
+{
+    class Program
+    {
+        public static int NumberOfSteps(int num)
+        {
+            int count = 0;
+            int x = num;
+
+            while (x > 0)
+            {
+                ++count;
+
+                if (x % 2 > 0)
+                {
+                    //Console.WriteLine("Step {0}) {1} is odd. Subtract by 1 and obtain by {2}", count, x, x - 1);
+                    x--;
+                    //count++;
+                }
+                else
+                {
+                    //Console.WriteLine("Step {0}) {1} is even. Divide by 2 and obtain by {1}", count, x, x - 1);
+                    x /= 2;
+                    //count++;
+                }
+
+
+            }
+            Console.WriteLine(count);
+            return count;
+        }
+
+        static void Main(string[] args)
+        { 
+            NumberOfSteps(int.Parse(Console.ReadLine()));
+
+        }
+    }
+    
+}
